e1cf5029643bb645af71958407cb230e8cf53c02
Sele <selene199520@gmail.com>
2019-11-29 12:47:49 -0300

    change

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Select from 'react-select'
-import { KeyboardDatePicker, } from '@material-ui/pickers';
 class SearchComponent extends React.Component {
     state = {
         date: new Date()
@@ -8,7 +7,6 @@ class SearchComponent extends React.Component {
 
 
     render() {
-        const { date } = this.state
         const options = [
             { value: 'chocolate', label: 'Chocolate' },
             { value: 'strawberry', label: 'Strawberry' },
@@ -22,23 +20,7 @@ class SearchComponent extends React.Component {
                 </h1>
                 <Select options={options} />
 
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-
-                    <KeyboardDatePicker
-                        disableToolbar
-                        variant="inline"
-                        format="MM/dd/yyyy"
-                        margin="normal"
-                        id="date-picker-inline"
-                        label="Date picker inline"
-                        value={selectedDate}
-                        onChange={handleDateChange}
-                        KeyboardButtonProps={{
-                            'aria-label': 'change date',
-                        }}
-                    />
-
-                </MuiPickersUtilsProvider>
+            
             </div>
         )
     }
